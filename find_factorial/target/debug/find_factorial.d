ddf2a82fb55f3427887b6cb9c7fb8fc1a227b399
afethi <fethiabderrahmane1@gmail.com>
2025-07-24 12:15:08 +0100

    factorial

--- find_factorial/target/debug/find_factorial.d
+++
@@ -1,1 +0,0 @@
-/home/afethi/piscine-rust/find_factorial/target/debug/find_factorial: /home/afethi/piscine-rust/find_factorial/src/lib.rs /home/afethi/piscine-rust/find_factorial/src/main.rs
--- find_factorial/target/debug/libfind_factorial.d
+++
@@ -1,1 +0,0 @@
-/home/afethi/piscine-rust/find_factorial/target/debug/libfind_factorial.rlib: /home/afethi/piscine-rust/find_factorial/src/lib.rs
