7c48d6e65ce9886771f2cc15b6eadfe8698534b9
afethi <fethiabderrahmane1@gmail.com>
2025-07-24 11:22:20 +0100

    groceries

---
+++ groceries/target/debug/deps/groceries-08b6009edf936a6f.d
@@ -0,0 +1,7 @@
+/home/afethi/piscine-rust/groceries/target/debug/deps/groceries-08b6009edf936a6f.d: src/lib.rs
+
+/home/afethi/piscine-rust/groceries/target/debug/deps/libgroceries-08b6009edf936a6f.rlib: src/lib.rs
+
+/home/afethi/piscine-rust/groceries/target/debug/deps/libgroceries-08b6009edf936a6f.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ groceries/target/debug/groceries.d
@@ -0,0 +1,1 @@
+/home/afethi/piscine-rust/groceries/target/debug/groceries: /home/afethi/piscine-rust/groceries/src/lib.rs /home/afethi/piscine-rust/groceries/src/main.rs
