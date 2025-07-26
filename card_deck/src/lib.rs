bd9ddaa701aeed575d805118792840393475f522
A-fethi <fethiabderrahmane1@gmail.com>
2025-07-26 22:54:53 +0100

    card deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,3 +1,6 @@
+use rand::Rng;
+
+#[derive(Debug, PartialEq)]
 pub enum Suit {
     Heart,
     Diamond,
@@ -5,6 +8,7 @@ pub enum Suit {
     Club,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
     Number(u8),
@@ -15,20 +19,49 @@ pub enum Rank {
 
 impl Suit {
     pub fn random() -> Suit {
+        let value = rand::rng().random_range(1..=4);
+        Suit::translate(value)
     }
 
     pub fn translate(value: u8) -> Suit {
+        if value == 1 {
+            Suit::Heart
+        } else if value == 2 {
+            Suit::Diamond
+        } else if value == 3 {
+            Suit::Spade
+        } else if value == 4 {
+            Suit::Club
+        } else {
+            panic!("Invalid value for Suit");
+        }
     }
 }
 
 impl Rank {
-    pub fn random() -> Rank {
+   pub fn random() -> Rank {
+        let value = rand::rng().random_range(1..=13);
+        Rank::translate(value)
     }
 
     pub fn translate(value: u8) -> Rank {
+        if value == 1 {
+            Rank::Ace
+        } else if value >= 2 && value <= 10 {
+            Rank::Number(value)
+        } else if value == 11 {
+            Rank::Jack
+        } else if value == 12 {
+            Rank::Queen
+        } else if value == 13 {
+            Rank::King
+        } else {
+            panic!("Invalid value for Rank");
+        }
     }
 }
 
+#[derive(Debug)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
